de666805be61af77c0072d433e793a0c83d3eeeb
Hardik Chauhan <hardik.chauhan@sarvadhi.com>
2022-11-23 10:16:45 +0530

    create card

--- src/Components/ProjectList.tsx
+++ src/Components/ProjectList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Project } from "./Project";
+import ProjectCard from "./ProjectCard";
 
 interface ProjectListProps {
   projects: Project[];
@@ -10,17 +11,8 @@ function ProjectList({ projects }: ProjectListProps) {
   return (
     <ul className="row">
       {projects.map((project) => (
-        <div className="cols-sm">
-          <div className="card">
-            <img src={project.imageUrl} alt={project.name} />
-            <section className="section dark">
-              <h5 className="strong">
-                <strong>{project.name}</strong>
-              </h5>
-              <p>{project.description}</p>
-              <p>Budget : {project.budget}</p>
-            </section>
-          </div>
+        <div key={project.id} className="cols-sm">
+          <ProjectCard project={project} />
         </div>
       ))}
     </ul>
