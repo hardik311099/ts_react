ba233d79ec1af57220101e1dd9a72febfe7e0647
Hardik Chauhan <hardik.chauhan@sarvadhi.com>
2022-11-23 09:54:49 +0530

    Listing Data

---
+++ src/Components/MockProject.ts
@@ -0,0 +1,114 @@
+import { Project } from "./Project";
+
+export const MOCK_PROJECT = [
+  new Project({
+    id: 1,
+    name: "Johnson - Kutch",
+    description:
+      "Fully-configurable intermediate framework. Ullam occaecati libero laudantium nihil voluptas omnis.",
+    imageUrl: "/assets/placeimg_500_300_arch4.jpg",
+    contractTypeId: 3,
+    contractSignedOn: "2013-08-04T22:39:41.473Z",
+    budget: 54637,
+    isActive: false,
+  }),
+  new Project({
+    id: 2,
+    name: "Wisozk Group",
+    description:
+      "Centralized interactive application. Exercitationem nulla ut ipsam vero quasi enim quos doloribus voluptatibus.",
+    imageUrl: "/assets/placeimg_500_300_arch1.jpg",
+    contractTypeId: 4,
+    contractSignedOn: "2012-08-06T21:21:31.419Z",
+    budget: 91638,
+    isActive: true,
+  }),
+  new Project({
+    id: 3,
+    name: "Denesik LLC",
+    description:
+      "Re-contextualized dynamic moratorium. Aut nulla soluta numquam qui dolor architecto et facere dolores.",
+    imageUrl: "/assets/placeimg_500_300_arch12.jpg",
+    contractTypeId: 6,
+    contractSignedOn: "2016-06-26T18:24:01.706Z",
+    budget: 29729,
+    isActive: true,
+  }),
+  new Project({
+    id: 4,
+    name: "Purdy, Keeling and Smitham",
+    description:
+      "Innovative 6th generation model. Perferendis libero qui iusto et ullam cum sint molestias vel.",
+    imageUrl: "/assets/placeimg_500_300_arch5.jpg",
+    contractTypeId: 4,
+    contractSignedOn: "2013-05-26T01:10:42.344Z",
+    budget: 45660,
+    isActive: true,
+  }),
+  new Project({
+    id: 5,
+    name: "Kreiger - Waelchi",
+    description:
+      "Managed logistical migration. Qui quod praesentium accusamus eos hic non error modi et.",
+    imageUrl: "/assets/placeimg_500_300_arch12.jpg",
+    contractTypeId: 2,
+    contractSignedOn: "2009-12-18T21:46:47.944Z",
+    budget: 81188,
+    isActive: true,
+  }),
+  new Project({
+    id: 6,
+    name: "Lesch - Waelchi",
+    description:
+      "Profound mobile project. Rem consequatur laborum explicabo sint odit et illo voluptas expedita.",
+    imageUrl: "/assets/placeimg_500_300_arch1.jpg",
+    contractTypeId: 3,
+    contractSignedOn: "2016-09-23T21:27:25.035Z",
+    budget: 53407,
+    isActive: false,
+  }),
+  new Project({
+    id: 7,
+    name: "Christiansen LLC",
+    description:
+      "Customer-focused composite implementation. Rerum ullam est voluptate hic eligendi autem rerum cum et.",
+    imageUrl: "/assets/placeimg_500_300_arch9.jpg",
+    contractTypeId: 3,
+    contractSignedOn: "2015-04-14T19:46:53.146Z",
+    budget: 98600,
+    isActive: true,
+  }),
+  new Project({
+    id: 8,
+    name: "Halvorson, Fadel and Zieme",
+    description:
+      "Triple-buffered transitional encoding. Amet illum quis expedita ut repudiandae et aut sit quia.",
+    imageUrl: "/assets/placeimg_500_300_arch1.jpg",
+    contractTypeId: 5,
+    contractSignedOn: "2011-10-16T08:07:32.217Z",
+    budget: 21205,
+    isActive: true,
+  }),
+  new Project({
+    id: 9,
+    name: "Lesch - Waelchi",
+    description:
+      "Profound mobile project. Rem consequatur laborum explicabo sint odit et illo voluptas expedita.",
+    imageUrl: "/assets/placeimg_500_300_arch1.jpg",
+    contractTypeId: 3,
+    contractSignedOn: "2016-09-23T21:27:25.035Z",
+    budget: 53407,
+    isActive: false,
+  }),
+  new Project({
+    id: 10,
+    name: "Kihn and Sons",
+    description:
+      "Multi-layered grid-enabled local area network. Accusamus numquam neque incidunt perspiciatis consequuntur beatae excepturi iure officiis.",
+    imageUrl: "/assets/placeimg_500_300_arch3.jpg",
+    contractTypeId: 4,
+    contractSignedOn: "2016-04-02T02:40:48.906Z",
+    budget: 62703,
+    isActive: false,
+  }),
+];
---
+++ src/Components/ProjectPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { MOCK_PROJECT } from "./MockProject";
+import ProjectList from "./ProjectList";
+
+function ProjectPage() {
+  return (
+    <>
+      <h1>Project</h1>
+      {/* <pre>{JSON.stringify(MOCK_PROJECT, null, "")}</pre> */}
+      <ProjectList projects={MOCK_PROJECT} />
+    </>
+  );
+}
+
+export default ProjectPage;
